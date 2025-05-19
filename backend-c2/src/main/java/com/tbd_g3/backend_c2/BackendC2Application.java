785fb98d0e03e8dfdf6216d21a66ff666f491ce7
Isidora Reveco <164648232+IsidoraSRM@users.noreply.github.com>
2025-05-19 17:58:46 -0400

    Backend

---
+++ backend-c2/src/main/java/com/tbd_g3/backend_c2/BackendC2Application.java
@@ -0,0 +1,13 @@
+package com.tbd_g3.backend_c2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendC2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendC2Application.class, args);
+	}
+
+}
---
+++ backend-c2/src/test/java/com/tbd_g3/backend_c2/BackendC2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tbd_g3.backend_c2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendC2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
